e492754b4e4913d0ca64c493e093405b77c2cf63
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-10-26 23:52:45 -0300

    added swagger plugin on nest-cli.json

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ConflictInterceptor } from './commom/errors/interceptos/conflict.interceptor';
 import { DatabaseInterceptor } from './commom/errors/interceptos/database.interceptor';
@@ -10,6 +11,16 @@ const PORT = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Simple Blog')
+    .setDescription('The simple Blog API description')
+    .setVersion('1.0')
+    .addTag('blog')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
