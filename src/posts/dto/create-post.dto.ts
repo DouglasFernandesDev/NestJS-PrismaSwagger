6cda4b628752b06b2f7f657d0f11449ab25b2bc2
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-10-27 00:14:10 -0300

    added schemas on swagger open api

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreatePostDto {
+  @ApiProperty({ description: 'Título do post é requerido', required: true })
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({ description: 'Conteúdo do post é opcional' })
   @IsString()
   @IsOptional()
   content?: string;
 
+  @ApiProperty({ description: 'Email do autor do post', required: true })
   @IsEmail()
   authorEmail: string;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({ description: 'Email do usuário', required: true })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({ description: 'Nome completo do usuário', required: true })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'Define se o usuário é administrador',
+    default: false,
+  })
   @IsBoolean()
   admin: boolean;
 }
